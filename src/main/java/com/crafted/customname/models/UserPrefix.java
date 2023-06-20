4febb9133d7034d72b411e5323bc607fd14316de
GAsplund <gasplund2@gmail.com>
2023-06-20 14:31:49 +0200

    Use Vault persistent data store for prefixes

--- src/main/java/com/crafted/customname/models/UserPrefix.java
+++ src/main/java/com/crafted/customname/models/UserPrefix.java
@@ -1,12 +1,38 @@
 package com.crafted.customname.models;
 
-public record UserPrefix(String title, String color) {
+public class UserPrefix {
+
+    private String title;
+    private String color;
+
+    public UserPrefix(String title, String color) {
+        this.title = title;
+        this.color = color;
+    }
+
     public UserPrefix() {
         this("", "&r");
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     @Override
     public String toString() {
         return title + (title.isEmpty() ? "" : " ") + color;
+    }
+
+    public String serialize() {
+        return title + "\n" + color;
+    }
+
+    public static UserPrefix deserialize(String serialized) {
+        String[] split = serialized.split("\n");
+        return new UserPrefix(split[0], split[1]);
     }
 }
--- src/main/java/com/crafted/customname/util/PrefixBuilder.java
+++ src/main/java/com/crafted/customname/util/PrefixBuilder.java
@@ -20,8 +20,9 @@ public class PrefixBuilder {
         chat = Objects.requireNonNull(Bukkit.getServer().getServicesManager().getRegistration(Chat.class)).getProvider();
     }
 
-    // Non-persistent for now
-    private static final Map<Player, UserPrefix> prefixes = new HashMap<>();
+    public UserPrefix getPrefix(Player player) {
+        return UserPrefix.deserialize(chat.getPlayerInfoString(player, "prefix_serialized", "\n&r"));
+    }
 
     /**
      * Sets the title prefix of a player
@@ -29,15 +30,9 @@ public class PrefixBuilder {
      * @param title The title to be set
      */
     public void setTitle(Player player, String title) {
-        UserPrefix prefix;
-        if (prefixes.containsKey(player)) {
-            prefix = prefixes.get(player);
-            prefix = new UserPrefix(title, prefix.color());
-        } else {
-            prefix = new UserPrefix(title, "&r");
-        }
-        prefixes.put(player, prefix);
-        applyPrefix(player);
+        UserPrefix prefix = getPrefix(player);
+        prefix.setTitle(title);
+        setPrefix(player, prefix);
     }
 
     /**
@@ -46,15 +41,9 @@ public class PrefixBuilder {
      * @param color The color to be set
      */
     public void setColor(Player player, String color) {
-        UserPrefix prefix;
-        if (prefixes.containsKey(player)) {
-            prefix = prefixes.get(player);
-            prefix = new UserPrefix(prefix.title(), color);
-        } else {
-            prefix = new UserPrefix("", color);
-        }
-        prefixes.put(player, prefix);
-        applyPrefix(player);
+        UserPrefix prefix = getPrefix(player);
+        prefix.setColor(color);
+        setPrefix(player, prefix);
     }
 
     /**
@@ -62,19 +51,15 @@ public class PrefixBuilder {
      * @param player The player to remove the prefix from
      */
     public void removePrefix(Player player) {
-        prefixes.put(player, new UserPrefix());
-        applyPrefix(player);
+        setPrefix(player, new UserPrefix("", "&r"));
     }
 
     /**
      * Applies the prefix to a player from stored data to LuckPerms
      * @param player The player to apply the prefix to
      */
-    public void applyPrefix(Player player) {
-        UserPrefix prefix = prefixes.get(player);
-        if (prefix == null) {
-            prefix = new UserPrefix("", "&r");
-        }
+    public void setPrefix(Player player, UserPrefix prefix) {
         chat.setPlayerPrefix(null, player, prefix.toString());
+        chat.setPlayerInfoString(player, "prefix_serialized", prefix.serialize());
     }
 }
