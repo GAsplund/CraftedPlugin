8ab72e617e2900d4f04b693c1ddb22218dd75128
GAsplund <gasplund2@gmail.com>
2023-06-24 16:15:49 +0200

    Change direction, remove info message
    WorldGuard will handle the info message instead

--- src/main/java/com/crafted/customname/commands/PortCommand.java
+++ src/main/java/com/crafted/customname/commands/PortCommand.java
@@ -14,18 +14,14 @@ public class PortCommand implements CommandExecutor {
     private final Component portMessage = Component
             .text("Wooosh! You have been teleported to port!")
             .color(NamedTextColor.DARK_GREEN);
-    private final Component infoMessage = Component
-            .text("** If you need some informations, follow the road without any wools.")
-            .color(NamedTextColor.AQUA);
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (sender instanceof Player player) {
-            Location loc = new Location(Bukkit.getWorld("2023_PORT"), 205.5, 64, 318.5, 0, 0);
+            Location loc = new Location(Bukkit.getWorld("2023_PORT"), 205.5, 64, 318.5, 180, 0);
             player.sendMessage(portMessage);
             player.teleport(loc);
-            player.sendMessage(infoMessage);
             return true;
         }
 
